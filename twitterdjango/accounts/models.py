d34fdbb95fe31f91b02faf5263eadfb22a9cc454
kahil nayton <kahilnayton@gmail.com>
2019-11-29 20:48:12 -0500

    set up the framework for auth and user login

--- twitterdjango/accounts/models.py
+++ twitterdjango/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
+class User(auth.models.User, auth.models.PermissionsMixin): # django's inbuilt auth
+  def __str__(self):
+    return "@{}.format(self.username)"
