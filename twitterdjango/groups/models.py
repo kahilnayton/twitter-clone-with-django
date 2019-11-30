e84ab6a7cf2e4d7702976b78d88fcc219be3c909
kahil nayton <kahilnayton@gmail.com>
2019-11-30 14:23:56 -0500

    finished off templates for groups

--- twitterdjango/groups/models.py
+++ twitterdjango/groups/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
 from django.utils.text import slugify # allows us to remove any symbols that are not Alphanumeric
 
 # Create your models here.
@@ -10,33 +12,36 @@ User = get_user_model()
 from django import template
 register = template.Library()
 
+
 class Group(models.Model):
-  name = models.CharField(max_length=255, unique=True)
-  slug = models.SlugField(allow_unicode=True, unique=True)
-  description = models.TextField(blank=True, default=' ')
-  description_html - models.TextField(editable=False, default='', blank=True)
-  members = models.ManyToManyField(User,through='GroupMember')
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User,through="GroupMember")
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-  def save(self, *args, **kwargs):
-    self.slug = slugify(self.name)
-    self.description_html = misaka.html(self.description)
-    super().save(*args, **kwargs)
-    
-  def get_absolute_url(self):
-    return reverse('groups:single', kwargs={'slug': self.slug})
-    
-  class Meta:
-    ordering = ['name']
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
+
+
+    class Meta:
+        ordering = ["name"]
+
 
 class GroupMember(models.Model):
-  group = models.ForeignKey(Group, related_name='memberships')
-  user = models.ForeignKey(User, related_name='user_group')
+    group = models.ForeignKey(Group,related_name='memberships',on_delete=models.CASCADE)
+    user = models.ForeignKey(User,related_name='user_groups',on_delete=models.CASCADE)
 
-  def __str__(self):
-    return self.user.username 
+    def __str__(self):
+        return self.user.username
 
-  class Meta:
-    unique_together = ('groups', 'user')
+    class Meta:
+        unique_together = ("group", "user")
